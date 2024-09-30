dd8fec2a92061b60384c34ef23909919272b4b41
meschinidino <francodmsoloing@gmail.com>
2024-09-29 21:23:55 -0300

    agregando alumnos + profesores para requisitos tarea

--- src/main/java/org/example/dino_jta/DinoJtaApplication.java
+++ src/main/java/org/example/dino_jta/DinoJtaApplication.java
@@ -1,6 +1,8 @@
 package org.example.dino_jta;
 
+import org.example.dino_jta.entidades.Alumno;
 import org.example.dino_jta.entidades.Profesor;
+import org.example.dino_jta.enumeraciones.Especialidades;
 import org.example.dino_jta.enumeraciones.Titulos;
 import org.example.dino_jta.repositorios.AlumnoRepository;
 import org.example.dino_jta.repositorios.PersonaRepository;
@@ -57,7 +59,48 @@ public class DinoJtaApplication {
 			// Lo grabo a través del repositorio de Spring
 	profesorRepository.save(pro1);
 
+			Profesor pro2 = Profesor.builder()
+					.nombre("NotAlberto")
+					.nombre("Cortez")
+					.fechaIngreso(LocalDate.of(2022, 1, 1))
+					.cantHijos(2)
+					.titulo(Titulos.MASTER)
+					.sueldo(new BigDecimal("123.45"))
+					.build();
 
+
+			// Lo grabo a través del repositorio de Spring
+			profesorRepository.save(pro2);
+
+			Alumno al1 = Alumno.builder()
+					.nombre("Dino")
+					.apellido("Meschini")
+					.legajo(62000)
+					.especialidad(Especialidades.BACHILLER)
+					.build();
+
+			// Lo grabo a través del repositorio de Spring
+			alumnoRepository.save(al1);
+
+			Alumno al2 = Alumno.builder()
+					.nombre("Dinon't")
+					.apellido("Meschini")
+					.legajo(62001)
+					.especialidad(Especialidades.BACHILLER)
+					.build();
+
+			// Lo grabo a través del repositorio de Spring
+			alumnoRepository.save(al2);
+
+			Alumno al3 = Alumno.builder()
+					.nombre("Alan")
+					.apellido("Turing")
+					.legajo(1)
+					.especialidad(Especialidades.BACHILLER)
+					.build();
+
+			// Lo grabo a través del repositorio de Spring
+			alumnoRepository.save(al3);
 
 
 
